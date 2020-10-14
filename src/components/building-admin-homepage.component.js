8b500a53ad2c7cc3860d734fc5785d11f5b1d8b7
Jonathan Hillier <jonhillier@Jonathans-MacBook-Pro.local>
2020-10-14 16:19:39 -0500

    Started dynamic info

--- src/components/building-admin-homepage.component.js
+++ src/components/building-admin-homepage.component.js
@@ -6,19 +6,42 @@ import   NavigationBar  from './navbar.component.js';
 export default class BuildingAdminDash extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            building_admin: '',
+            num_floors: '',
+            capacity: '',
+            address: '',
+            users: []
+          }
+    }
+
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/users/')
+          .then(response => {
+              if (response.data.length > 0) {
+                  this.setState({
+                      users: response.data.map(user => user._id),
+                      building_admin: response.data[0]._id
+                      //get office managers and then create list with their company names and their floor numbers
+                  })
+              }
+          })
+          .catch((error) => {
+              console.log(error);
+          })
     }
 
     render() {
         return (
             <div>
-                <NavigationBar />
                 <ul>
-                    <li>
-                        Hello
-                    </li>
-                    <li>
-                        Hello2
-                    </li>
+                    {this.state.users.map(user => 
+                        <li key={user.email}>
+                            {user._id}
+                        </li>
+                    )}
                 </ul>
             </div>
         )
