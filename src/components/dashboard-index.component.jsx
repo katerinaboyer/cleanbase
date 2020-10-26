e3d475d3c47d59af7f7aa3920b01f1e92bf17829
Cole Heath <cole.heath@tamu.edu>
2020-10-26 00:33:53 -0500

    add sanitation dashboard

--- src/components/dashboard-index.component.jsx
+++ src/components/dashboard-index.component.jsx
@@ -5,6 +5,7 @@ import { useHistory } from 'react-router-dom';
 import BuildingAdminDash from './building-admin-homepage.component';
 import DashboardManager from './dashboard-manager.component';
 import DashboardUser from './dashboard-user.component';
+import DashboardSanitation from './dashboard-sanitation.component';
 
 const DashboardIndex = (props) => {
   const history = useHistory();
@@ -16,6 +17,8 @@ const DashboardIndex = (props) => {
       return <DashboardManager />;
     case 'employee':
       return <DashboardUser />;
+    case 'sanitation':
+      return <DashboardSanitation />;
     default:
       history.push('/');
       return <div />;
