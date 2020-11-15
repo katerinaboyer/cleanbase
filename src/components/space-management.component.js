58569d8e1e7a69a83f8cc6bad4ab5b89b78ff358
Jonathan Hillier <jonhillier@Jonathans-MBP.lan>
2020-11-15 11:11:29 -0600

    axios request update

--- src/components/space-management.component.js
+++ src/components/space-management.component.js
@@ -5,15 +5,21 @@ import './../styles.css';
 import IllnessReport from "./illness-report.component";
 import { Container, Row, Col, Form, Card} from 'react-bootstrap';
 import { useHistory } from "react-router-dom";
-import {connect} from "react-redux";
+import {connect, useSelector} from "react-redux";
 import { setAccountId } from "../store/businessAccountReducer";
+import { getUser } from "../store/selectors";
 
 
 const SpaceMgmt = (props) => {
 
   const [accounts, setAccounts] = useState([]);
+  const [rooms, setRooms] = useState([]);
+  const [desks, setDesks] = useState([]);
+  //setAccounts([]);
+
 
   const history = useHistory();
+  const user = useSelector(getUser);
 
   const addAccount = (e) =>{
       history.push("/account");
@@ -35,78 +41,46 @@ const getFloorNumbers = (floors) => {
 
   useEffect(() => {
       async function fetchData() {
-          axios.get('http://localhost:5000/accounts/')
+          axios.get('http://localhost:5000/accounts/officemanager/' + user._id)
           .then(response => {
-              //console.log(response.data.length);
-              //console.log(response.data);
-              if (response.data.length > 0) {
-                  setAccounts(response.data);
+              console.log(response.data[0].floors_assigned);
+              for(var i = 0; i < response.data[0].floors_assigned.length; i++){
+                axios.get('http://localhost:5000/floors/id/' + response.data[0].floors_assigned[i])
+                .then(response => {
+                    console.log(response.data);
+                    for(var i = 0; i < response.data.room_list.length; i++){
+                        axios.get('http://localhost:5000/rooms/id/' + response.data.room_list[i])
+                        .then(response => {
+                            setRooms(response.data);
+                        })
+                        .catch((error) => {
+                            console.log(error);
+                        })
+                        axios.get('http://localhost:5000/desks/byrooom/' + response.data.room_list[i])
+                        .then(response => {
+                            setDesks(response.data);
+                        })
+                        .catch((error) => {
+                            console.log(error);
+                        })
+                    }
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
               }
           })
           .catch((error) => {
               console.log(error);
           })
+
       }
       fetchData();
   },[]);
 
   return (
       <div style={{padding:"30px 8%"}}>
-        <Row>
-        <Col>
-          <Row style={{paddingLeft:"3%"}}>
-            <Col sm={9}><h3>Desks</h3></Col>
-            <Col><button className="button-add" style={{marginLeft: "30%", marginRight: "50px"}} onClick={addAccount}>ADD</button></Col>
-          </Row>
-          <div style={{}}>
-              { accounts.map(info =>
-                  <div style={{paddingBottom:"20px"}}>
-                      <Card style={{borderRadius:"15px"}}>
-                              <Row>
-                                  <Col sm={3}>
-                                      <Card.Title style={{padding:"20px 0px 20px 25px", fontSize:"130%"}}>Floor 1</Card.Title>
-                                  </Col>
-                                  <Col style={{textAlign: "center"}}>
-                                      <Card.Text style={{color:"#434343", padding:"25px 0px 40px 0%", fontSize:"130%"}}>Desk</Card.Text>
-                                  </Col>
-                                  <Col sm={3} style={{}}>
-                                      <button className="button-edit" style={{marginLeft:"50%", fontSize:"130%"}} onClick={() => test(info)}>Edit</button>
-                                  </Col>
-                              </Row>
-                      </Card>
-                  </div>
-                  )
-              }
-          </div>
-        </Col>
-        <Col sm={1}></Col>
-        <Col>
-          <Row style={{paddingLeft:"3%"}}>
-            <Col sm={9}><h3>Rooms</h3></Col>
-            <Col><button className="button-add" style={{marginLeft:"35%"}} onClick={addAccount}>ADD</button></Col>
-          </Row>
-          <div style={{}}>
-              { accounts.map(info =>
-                  <div style={{paddingBottom:"20px"}}>
-                      <Card style={{borderRadius:"15px"}}>
-                              <Row>
-                                  <Col sm={3}>
-                                      <Card.Title style={{padding:"20px 0px 20px 25px", fontSize:"130%"}}>Floor</Card.Title>
-                                  </Col>
-                                  <Col style={{textAlign: "center"}}>
-                                      <Card.Text style={{color:"#434343", padding:"25px 0px 40px 0%", fontSize:"130%"}}>Room</Card.Text>
-                                  </Col>
-                                  <Col sm={3} style={{}}>
-                                      <button className="button-edit" style={{marginLeft:"30%", fontSize:"130%"}}onClick={() => test(info)}>Remove</button>
-                                  </Col>
-                              </Row>
-                      </Card>
-                  </div>
-                  )
-              }
-          </div>
-        </Col>
-        </Row>
+        
       </div>
       )
 }
