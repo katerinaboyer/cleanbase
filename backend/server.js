3c87bca170296d84f0194097b8d0d624d1f80f0e
Jonathan Hillier <jonhillier@Jonathans-MacBook-Pro.local>
2020-11-17 12:24:47 -0600

    dont break

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
-);
+const uri = "mongodb+srv://jon:UJ62J1tfuqRuZ1WV@cluster0.qxidv.mongodb.net/<cb-test>?retryWrites=true&w=majority";
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
