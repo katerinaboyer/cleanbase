e944509b34066aa5c371559f116c0ad2112a3582
Jonathan Hillier <jonhillier@Jonathans-MacBook-Pro.local>
2020-11-18 10:30:16 -0600

    idk

--- src/components/create-room.component.js
+++ src/components/create-room.component.js
@@ -1,159 +1,16 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, Component} from "react";
 import axios from "axios";
 import { useHistory, useLocation } from "react-router-dom";
 import {connect, useSelector} from "react-redux";
 import {Form, Col, Row} from 'react-bootstrap';
 import { getUser } from "../store/selectors";
+import ToastMessage from './toast.component';
+
 
 const CreateRoom = (props) => {
-  const [room_number, setRoomNumber] = useState();
-  const [floor_id, setFloorId] = useState();
-  const [capacity, setCapacity] = useState();
-  const [room_type, setRoomType] = useState();
-  const [floors, setFloors] = useState([]);
-
-  var floorID = "";
-
   const user = useSelector(getUser);
 
-  useEffect(() =>{
-    setRoomType("desk_space")
-
-    var temp = [];
-
-    async function fetchData() {
-        axios.get("http://localhost:5000/accounts/office/" + user._id)
-      .then((response) => {
-          for(var j = 0; j < response.data[0].floors_assigned.length; j++){
-            axios.get("http://localhost:5000/floors/id/" + response.data[0].floors_assigned[j])
-            .then((response) => {
-              console.log(response.data);
-              if (response.data !== null) {
-                temp.push(response.data)
-                console.log(response.data._id);
-                setFloorId(response.data._id);
-                floorID = response.data._id;
-                console.log(floorID);
-                console.log(floor_id);
-              }
-            })
-            .catch((error) => {
-              console.log(error);
-            });
-          }
-          setFloors(temp);
-      });
-    }
-    fetchData();
-  },[]);
-
-  const onChangeRoomNumber= (e) => {
-    setRoomNumber(e.target.value);
-  }
-
-  const onChangeFloorId = (e) => {
-    setFloorId(e.target.value)
-    floorID = e.target.value;
-    console.log(e.target.value);
-    console.log(floorID);
-  }
-
-  const onChangeCapacity= (e) => {
-    setCapacity(e.target.value)
-  }
-
-  const onChangeRoomType = (e) => {
-    setRoomType(e.target.value)
-  }
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-
-    const newRoom = {
-      room_number: room_number,
-      floor_id: floorID,
-      capacity: capacity,
-      room_type: room_type,
-    };
-
-    var tempRooms = [];
-
-    for(var i = 0; i < floors.length; i++){
-      if(floors[i]._id === floor_id._id){
-        tempRooms = floors[i].room_list;
-      }
-    }
-
-    console.log(tempRooms);
-
-    axios.post("http://localhost:5000/rooms/add", newRoom)
-      .then((res) => {
-        console.log(res.data);
-        axios.get('http://localhost:5000/rooms/')
-        .then((res) => {
-          tempRooms.push(res.data[res.data.length-1]._id);
-          const updateFloor = {
-            room_list: tempRooms
-          };
-          console.log(updateFloor);
-          console.log(floorID);
-          axios.post('http://localhost:5000/floors/update/' + floorID, updateFloor)
-          .then((res) => console.log(res.data));
-        });
-      });
-  }
-
-  return (
-    <div style={{marginLeft:"10.5rem", display:"block", color:"white", width:"45%"}} >
-      <h3 className="h3">Create New Room:</h3>
-      <Form onSubmit={onSubmit}>
-          <Form.Group as={Row} controlId="formAdmin">
-              <Form.Label column sm={3}>Room Number</Form.Label>
-              <Col sm={9}>
-                  <Form.Control type="name" placeholder="12A" onChange={onChangeRoomNumber}/>
-              </Col>
-          </Form.Group>
-
-          <Form.Group as={Row} controlId="formAdmin">
-              <Form.Label column sm={3}>Capacity</Form.Label>
-              <Col sm={9}>
-                  <Form.Control type="name" placeholder="12" onChange={onChangeCapacity}/>
-              </Col>
-          </Form.Group>
-
-          <Form.Group as={Row} controlId="formFloorNumbers">
-              <Form.Label column sm={3}>Room Type</Form.Label>
-              <Col sm={9}>
-                  <Form.Control as="select" onChange={onChangeRoomType}>
-                    <option value="desk_space">Desk Space</option>
-                    <option value="office">Office</option>
-                    <option value="conference">Conference</option>
-                  </Form.Control>
-              </Col>
-          </Form.Group>
-
-          <Form.Group as={Row} controlId="formFloorNumbers">
-              <Form.Label column sm={3}>Floor Number</Form.Label>
-              <Col sm={9}>
-                  <Form.Control as="select" onChange={onChangeFloorId}>
-                  {floors.map((floor) => {
-                    return (
-                      <option key={floor._id} value={floor._id}>
-                        {floor.floor_number}
-                      </option>
-                    );
-                  })}
-                  </Form.Control>
-              </Col>
-          </Form.Group>
-
-          <button className="button-submit" type="submit">
-              Create Room
-          </button>
-      </Form>
-    </div>
-   
-  );
+  return(<RoomForm user={user}/>)
 
 }
 
@@ -161,10 +18,7 @@ const mapStateToProps = (state) => {return state};
 
 export default connect(mapStateToProps)(CreateRoom);
 
-/*
-import ToastMessage from './toast.component';
-
-export default class CreateRoom extends Component {
+class RoomForm extends Component {
   constructor(props) {
     super(props);
 
@@ -182,17 +36,17 @@ export default class CreateRoom extends Component {
       floors: [],
       show_error: false,
       show_success: false,
+      user: props.user._id
     };
   }
 
   componentDidMount() {
-    axios.get("http://localhost:5000/accounts/floors/" + )
-    axios.get("http://localhost:5000/floors/")
-      .then((response) => {
-        if (response.data.length > 0) {
+      axios.get("http://localhost:5000/floors/")
+      .then((res) => {
+        if (res.data.length > 0) {
           this.setState({
-            floors: response.data.map((floor) => floor),
-            floor_id: response.data[0],
+            floors: res.data.map((floor) => floor),
+            floor_id: res.data[0],
           });
           console.log(this.state.floors);
         }
@@ -236,17 +90,36 @@ export default class CreateRoom extends Component {
       this.state.room_type
     ) {
       const newRoom = {
-        room_number: this.state.room_number,
-        floor_id: this.state.floor_id,
-        capacity: this.state.capacity,
-        room_type: this.state.room_type,
+        room_number: this.room_number,
+        floor_id: this.floor_id,
+        capacity: this.capacity,
+        room_type: this.room_type,
       };
-
+  
+      var tempRooms = [];
+      /*
+      for(var i = 0; i < this.floors.length; i++){
+        if(this.floors[i]._id === this.floor_id._id){
+          tempRooms = this.floors[i].room_list;
+        }
+      }*/
+  
       console.log(newRoom);
-
-      axios
-        .post("http://localhost:5000/rooms/add", newRoom)
-        .then((res) => console.log(res.data));
+  
+      axios.post("http://localhost:5000/rooms/add", newRoom)
+        .then((res) => {
+          console.log(res.data);
+          axios.get('http://localhost:5000/rooms/')
+          .then((res) => {
+            tempRooms.push(res.data[res.data.length-1]._id);
+            const updateFloor = {
+              room_list: tempRooms
+            };
+            console.log(updateFloor);
+            axios.post('http://localhost:5000/floors/update/' + this.floor_id, updateFloor)
+            .then((res) => console.log(res.data));
+          });
+        });
 
         this.setState({
           show_success: true
@@ -322,4 +195,3 @@ export default class CreateRoom extends Component {
     );
   }
 }
-*/
--- src/components/space-management.component.js
+++ src/components/space-management.component.js
@@ -14,11 +14,9 @@ const SpaceMgmt = (props) => {
   //const [accounts, setAccounts] = useState([]);
   const [rooms, setRooms] = useState([]);
   const [desks, setDesks] = useState([]);
+  const [value, setValue] = useState(0);
   //setAccounts([]);
-<<<<<<< HEAD
   const user = useSelector(getUser);
-=======
->>>>>>> dev
 
   const history = useHistory();
 
@@ -37,22 +35,34 @@ const SpaceMgmt = (props) => {
 }
 
 const getFloorNumbers = (floors) => {
-    const floorNum = [1,2,3];
-    const floorText = floorNum[0] + " - " + floorNum[floorNum.length-1];
+    axios.get("http://localhost:5000/floors/id/" + floors)
+    .then((response) => {
+        return response.data.floor_number
+    })
+    .catch((error) => {
+        return "NULL";
+    })
+}
 
-    return floorText;
+const getDeskFloorNumbers = (room) => {
+    axios.get("http://localhost:5000/desks/id/" + room)
+    .then((response) => {
+        return getFloorNumbers(response.data.floor_id);
+    })
+    .catch((error) => {
+        return "NULL";
+    })
+    setValue(room.length);
 }
 
 var tempRooms = [];
 var tempDesks= [];
 
   useEffect(() => {
-      async function fetchData() {
-          console.log(user._id);
-          axios.get('http://localhost:5000/accounts/office/' + user._id)
+      const fetchData = async () => {
+          await axios.get('http://localhost:5000/accounts/office/' + user._id)
           .then(response => {
-              console.log(response.data[0].floors_assigned);
-              console.log(response.data[0].business_name);
+              //console.log(response.data[0].floors_assigned);
               for(var i = 0; i < response.data[0].floors_assigned.length; i++){
                 axios.get('http://localhost:5000/floors/id/' + response.data[0].floors_assigned[i])
                 .then(response => {
@@ -60,17 +70,18 @@ var tempDesks= [];
                     for(var i = 0; i < response.data.room_list.length; i++){
                         axios.get('http://localhost:5000/rooms/id/' + response.data.room_list[i])
                         .then(response => {
+                            console.log(response.data);
                             tempRooms.push(response.data);
-                            //setRooms(response.data);
-                            console.log(rooms);
-                        })
-                        .catch((error) => {
-                            console.log(error);
-                        })
-                        axios.get('http://localhost:5000/desks/byrooom/' + response.data.room_list[i])
-                        .then(response => {
-                            tempDesks.push(response.data);
-                            //setDesks(response.data);
+                            axios.get('http://localhost:5000/desks/byroom/' + response.data._id)
+                            .then(response => {
+                                console.log(response.data);
+                                tempDesks.push(response.data);
+                                
+                            })
+                            .catch((error) => {
+                                console.log(error);
+                            })
+                            
                         })
                         .catch((error) => {
                             console.log(error);
@@ -90,7 +101,9 @@ var tempDesks= [];
 
       }
       fetchData();
-  },[]);
+      console.log(rooms);
+      console.log(desks);
+  },[value]);
 
   return (
     <div style={{padding:"30px 8%"}}>
@@ -98,7 +111,7 @@ var tempDesks= [];
         <Col>
           <Row style={{paddingLeft:"3%"}}>
             <Col sm={9}><h3>Desks</h3></Col>
-            <Col><button className="button-add" style={{marginLeft: "30%", marginRight: "50px"}} onClick={addDesk}>ADD</button></Col>
+            <Col><button className="button-add" style={{marginLeft: "30%", marginRight: "50px"}} onClick={addDesk}>{desks.length}</button></Col>
           </Row>
           <div style={{}}>
               { desks.map(info =>
@@ -106,10 +119,10 @@ var tempDesks= [];
                       <Card style={{borderRadius:"15px"}}>
                               <Row>
                                   <Col sm={3}>
-                                      <Card.Title style={{padding:"20px 0px 20px 25px", fontSize:"130%"}}>Floor 1</Card.Title>
+                                      <Card.Title style={{padding:"20px 0px 20px 25px", fontSize:"130%"}}>Floor {getDeskFloorNumbers(info.room_id)}</Card.Title>
                                   </Col>
                                   <Col style={{textAlign: "center"}}>
-                                      <Card.Text style={{color:"#434343", padding:"25px 0px 40px 0%", fontSize:"130%"}}>Desk</Card.Text>
+                                      <Card.Text style={{color:"#434343", padding:"25px 0px 40px 0%", fontSize:"130%"}}>Desk {info.desk_number}</Card.Text>
                                   </Col>
                                   <Col sm={3} style={{}}>
                                       <button className="button-edit" style={{marginLeft:"50%", fontSize:"130%"}} onClick={() => test(info)}>Edit</button>
@@ -125,7 +138,7 @@ var tempDesks= [];
         <Col>
           <Row style={{paddingLeft:"3%"}}>
             <Col sm={9}><h3>Rooms</h3></Col>
-            <Col><button className="button-add" style={{marginLeft:"35%"}} onClick={addRoom}>ADD</button></Col>
+            <Col><button className="button-add" style={{marginLeft:"35%"}} onClick={addRoom}>{rooms.length}</button></Col>
           </Row>
           <div style={{}}>
               { rooms.map(info =>
@@ -133,10 +146,10 @@ var tempDesks= [];
                       <Card style={{borderRadius:"15px"}}>
                               <Row>
                                   <Col sm={3}>
-                                      <Card.Title style={{padding:"20px 0px 20px 25px", fontSize:"130%"}}>Floor</Card.Title>
+                                      <Card.Title style={{padding:"20px 0px 20px 25px", fontSize:"130%"}}>Floor {getFloorNumbers(info.floor_id)}</Card.Title>
                                   </Col>
                                   <Col style={{textAlign: "center"}}>
-                                      <Card.Text style={{color:"#434343", padding:"25px 0px 40px 0%", fontSize:"130%"}}>Room</Card.Text>
+                                      <Card.Text style={{color:"#434343", padding:"25px 0px 40px 0%", fontSize:"130%"}}>Room: {info.room_number}</Card.Text>
                                   </Col>
                                   <Col sm={3} style={{}}>
                                       <button className="button-edit" style={{marginLeft:"30%", fontSize:"130%"}}onClick={() => test(info)}>Remove</button>
